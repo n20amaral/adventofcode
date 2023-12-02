3b8117879b645a6e68841aeac31ee075ab87232f
Nuno Amaral <namaral@mainhub.pt>
2023-12-02 11:55:49 +0000

    solved 2022 Day1

--- AoC2022/Day1.cs
+++ AoC2022/Day1.cs
@@ -1,4 +1,5 @@
-﻿using AoC.Common;
+﻿using System.Linq;
+using AoC.Common;
 
 namespace AoC2022;
 
@@ -11,14 +12,65 @@ public class Day1 : ExerciseBase
     public override string SolvePart1()
     {
         ResetReader();
+        var mostCarrying = 0;
+        var current = 0;
+        string? line;
 
-        return string.Empty;
+        while ((line = _reader.ReadLine()) != null)
+        {
+            if (!string.IsNullOrWhiteSpace(line))
+            {
+                current += int.Parse(line);
+                continue;
+            }
+
+            if (current > mostCarrying)
+            {
+                mostCarrying = current;
+            }
+
+            current = 0;
+        }
+
+        mostCarrying = current > mostCarrying ? current : mostCarrying;
+
+        return mostCarrying.ToString();
     }
 
     public override string SolvePart2()
     {
         ResetReader();
+        var topCarrying = new List<int>();
+        var current = 0;
+        string? line;
 
-        return string.Empty;
+        while ((line = _reader.ReadLine()) != null)
+        {
+            if (!string.IsNullOrWhiteSpace(line))
+            {
+                current += int.Parse(line);
+                continue;
+            }
+
+            if (topCarrying.Count < 3)
+            {
+                topCarrying.Add(current);
+            }
+            else if (current > topCarrying.Min())
+            {
+                topCarrying.Remove(topCarrying.Min());
+                topCarrying.Add(current);
+            }
+
+            current = 0;
+        }
+
+        if(current > topCarrying.Min())
+        {
+            topCarrying.Remove(topCarrying.Min());
+            topCarrying.Add(current);
+        }
+
+        return topCarrying.Sum().ToString();   
     }
 }
