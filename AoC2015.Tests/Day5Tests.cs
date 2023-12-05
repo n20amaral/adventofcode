95454f09f63aa0b8f2176daeda330e8ad6fbb07d
Nuno Amaral <namaral@mainhub.pt>
2023-12-05 00:54:10 +0000

    solved 2015 Day5

---
+++ AoC2015.Tests/Day5Tests.cs
@@ -0,0 +1,38 @@
+﻿namespace AoC2015.Tests;
+
+public class Day5Tests
+{
+
+    [Theory]
+    [InlineData("ugknbfddgicrmopn", "1")]
+    [InlineData("aaa", "1")]
+    [InlineData("jchzalrnumimnmhp", "0")]
+    [InlineData("haegwjzuvuyypxyu", "0")]
+    [InlineData("dvszwmarrgswjxmb", "0")]
+    public void Part1(string input, string expected)
+    {
+        var reader = new StringReader(input);
+        var day = new Day5(reader);
+
+        var actual = day.SolvePart1();
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData("qjhvhtzxzqqjkmpb", "1")]
+    [InlineData("xxyxx", "1")]
+    [InlineData("uurcxstgmygtbstg", "0")]
+    [InlineData("ieodomkazucvgmuy", "0")]
+    [InlineData("abaxyxy", "1")]
+    [InlineData("xxabaxxx", "1")]
+    public void Part2(string input, string expected)
+    {
+        var reader = new StringReader(input);
+        var day = new Day5(reader);
+
+        var actual = day.SolvePart2();
+
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ AoC2015/Day5.cs
@@ -0,0 +1,102 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices.Marshalling;
+using AoC.Common;
+
+namespace AoC2015;
+
+public class Day5 : ExerciseBase
+{
+    public Day5(TextReader reader) : base(reader)
+    {
+    }
+
+    public override string SolvePart1()
+    {
+        ResetReader();
+        var naughty = new string[] { "ab", "cd", "pq", "xy" };
+        var sum = 0;
+        string? word;
+
+        while((word = _reader.ReadLine()) != null)
+        {
+            if(string.IsNullOrWhiteSpace(word))
+            {
+                continue;
+            }
+;
+            var doubleCheck = false;
+            var naughtyCheck = false;
+            var vowelCount = 0;
+
+            for(var i = 0; i < word.Length; i++)
+            {
+                var notLastChar = i < word.Length - 1;
+
+                if(notLastChar && naughty.Contains($"{word[i]}{word[i + 1]}"))
+                {
+                    naughtyCheck = true;
+                    break;
+                }
+
+                if(!doubleCheck && notLastChar && word[i] == word[i + 1])
+                {
+                    doubleCheck = true;
+                }
+
+                switch(word[i])
+                {
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                        vowelCount++;
+                        break;
+                }
+            }
+
+            sum += !naughtyCheck && doubleCheck && vowelCount > 2 ? 1 : 0;
+        }
+
+        return sum.ToString();
+    }
+
+    public override string SolvePart2()
+    {
+        ResetReader();
+        var sum = 0;
+        string? word;
+
+        while((word = _reader.ReadLine()) != null)
+        {
+            if(string.IsNullOrWhiteSpace(word))
+            {
+                continue;
+            }
+
+            var inBetweenCheck = false;
+            var pairCheck = false;
+
+            for(var i = 0; i < word.Length - 1; i++)
+            {
+                if(!inBetweenCheck && i > 0)
+                {
+                    var slice = word.Substring(i - 1, 3);
+                    inBetweenCheck = slice[0] == slice[2] && slice[0] != slice[1];
+                }
+
+                // no space for a next pair, or no next pair found
+                if(i > word.Length - 4 || word.IndexOf($"{word[i]}{word[i + 1]}", i + 2) == - 1)
+                {
+                    continue;
+                }
+
+                pairCheck = true;
+            } 
+
+            sum += pairCheck && inBetweenCheck ? 1 : 0;
+        }
+
+        return sum.ToString();
+    }
+}
