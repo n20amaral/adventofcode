535ebf3a7d31802288e1bbbbf16fda9f6a1cce73
Nuno Amaral <n20amaral@outlook.com>
2023-12-06 17:12:20 -0100

    solved 2023 Day6

---
+++ AoC2023.Tests/Day6Tests.cs
@@ -0,0 +1,28 @@
+﻿namespace AoC2023.Tests;
+
+public class Day6Tests
+{
+    [Theory]
+    [InlineData("Time:      7  15   30\nDistance:  9  40  200", "288")]
+    public void SolvePart1(string input, string expected)
+    {
+        using var reader = new StringReader(input);
+        var day = new Day6(reader);
+
+        var actual = day.SolvePart1();
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData("Time:      7  15   30\nDistance:  9  40  200", "71503")]
+    public void SolvePart2(string input, string expected)
+    {
+        using var reader = new StringReader(input);
+        var day = new Day6(reader);
+
+        var actual = day.SolvePart2();
+
+        Assert.Equal(expected, actual);   
+    }
+}
---
+++ AoC2023/Day6.cs
@@ -0,0 +1,75 @@
+﻿using AoC.Common;
+
+namespace AoC2023;
+
+public class Day6 : ExerciseBase
+{
+    public Day6(TextReader reader) : base(reader)
+    {
+    }
+
+    public override string SolvePart1()
+    {
+        ResetReader();
+
+        var times = _reader.ReadLine()?
+            .Split(':')[1]
+            .Split(' ').Where(s => s.Length > 0).Select(long.Parse)
+            .ToArray();
+        _ = times ?? throw new Exception("Couldn't get time values");
+        var distances = _reader.ReadLine()?
+            .Split(':')[1]
+            .Split(' ').Where(s => s.Length > 0).Select(long.Parse)
+            .ToArray();
+        _ = distances ?? throw new Exception("Couldn't get distance values");
+        
+        var waysToBeat = new int[times.Length];
+
+        for (int i = 0; i < waysToBeat.Length; i++)
+        {
+            waysToBeat[i] = CountWaysToBeat(times[i], distances[i]);
+        }
+
+        return waysToBeat.Aggregate((a,b) => a*b).ToString();
+    }
+    public override string SolvePart2()
+    {
+        ResetReader();
+
+        var time = _reader.ReadLine()?
+            .Split(':')[1]
+            .Replace(" ", "") ?? string.Empty;
+            
+        var distance = _reader.ReadLine()?
+            .Split(':')[1]
+            .Replace(" ", "") ?? string.Empty;
+
+        return CountWaysToBeat(long.Parse(time), long.Parse(distance)).ToString();
+    }
+
+    private int CountWaysToBeat(long time, long distance)
+    {
+        var ways = 0;
+        var pressing = 0;
+
+        while(time - pressing > 0)
+        {
+            var timeAvailable = time - pressing;
+            var result = timeAvailable * pressing;
+
+            if(ways > 0 && result <= distance)
+            {
+                break;
+            }
+
+            if(result > distance)
+            {
+                ways++;
+            }
+
+            pressing++;
+        }
+
+        return ways;
+    }
+}
