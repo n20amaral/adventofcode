ebda8b27ed755220ae01d31934b8a622af0850a4
Nuno Amaral <namaral@mainhub.pt>
2023-12-08 14:46:10 +0000

    solved 2023 Day7

---
+++ AoC2023.Tests/Day7Tests.cs
@@ -0,0 +1,38 @@
+﻿namespace AoC2023.Tests;
+
+public class Day7Tests
+{
+
+    [Theory]
+    [InlineData("32T3K 765\nT55J5 684\nKK677 28\nKTJJT 220\nQQQJA 483", "6440")]
+    [InlineData("9QQQQ 1\nKKKKK 1000\nKQQQQ 10\nQQQQQ 100", "4321")]
+    [InlineData("99888 100\n97QQQ 1\n99QQQ 1000\n98QQQ 10", "4321")]
+    [InlineData("57Q79 10\n99Q77 1000\n99Q88 10000\n96Q79 100\n23456 1", "54321")]
+    public void SolvePart1(string input, string expected)
+    {
+        using var reader = new StringReader(input);
+        var day = new Day7(reader);
+
+        var actual = day.SolvePart1();
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData("32T3K 765\nT55J5 684\nKK677 28\nKTJJT 220\nQQQJA 483", "5905")]
+    [InlineData("9QQQQ 1\nKKKKK 1000\nKQQQQ 10\nQQQQQ 100", "4321")]
+    [InlineData("99888 100\n97QQQ 1\n99QQQ 1000\n98QQQ 10", "4321")]
+    [InlineData("57Q79 10\n99Q77 1000\n99Q88 10000\n96Q79 100\n23456 1", "54321")]
+    [InlineData("9QQQQ 1\nKKKKK 1000\nJQQQQ 10\nQQQQQ 100", "4321")]
+    [InlineData("99888 100\n97QQQ 1\n99QQQ 1000\nJ8QQQ 10", "3241")]
+    [InlineData("57Q79 10\n99QJJ 1000\n99Q88 10000\n96Q79 100\n23456 1", "45321")]
+    public void SolvePart2(string input, string expected)
+    {
+        using var reader = new StringReader(input);
+        var day = new Day7(reader);
+
+        var actual = day.SolvePart2();
+
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ AoC2023/Day7.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections.Immutable;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using AoC.Common;
+
+namespace AoC2023;
+
+public class Day7 : ExerciseBase
+{
+    public Day7(TextReader reader) : base(reader)
+    {
+    }
+
+    public override string SolvePart1()
+    {
+        var winnings = 0;
+        var games = ParseInput("23456789TJQKA", (hand) => 
+        {
+            var group = hand.GroupBy(c => c);
+
+            return [
+                group.Where(g => g.Count() == 5).Count(), // fifths
+                group.Where(g => g.Count() == 4).Count(), // quads
+                group.Any(g => g.Count() == 2) && group.Any(g => g.Count() == 3) ? 1 : 0, // full house
+                group.Where(g => g.Count() == 3).Count(), // triples
+                group.Where(g => g.Count() == 2).Count(), // pairs
+            ];
+        }).OrderDescending().ToArray();
+
+        for (int i = 0; i < games.Length; i++)
+        {
+            winnings += games[i].BidAmount * (i +1);
+        }
+
+        return winnings.ToString();
+    }
+
+    public override string SolvePart2()
+    {
+        var winnings = 0;
+        var games = ParseInput("J23456789TQKA", (hand) => 
+        {
+            var jokers = hand.Where(c => c == 0).Count();
+
+            var group = hand.GroupBy(c => c);
+            var fifths = group.Where(g => g.Count() == 5).Count();
+            var quads = group.Where(g => g.Count() == 4).Count();
+            var triples = group.Where(g => g.Count() == 3).Count();
+            var pairs = group.Where(g => g.Count() == 2).Count();
+            var fullHouse = triples > 0 && pairs > 0 ? 1 : 0;
+
+            return jokers switch {
+                5 => [fifths, 0, 0, 0, 0],
+                4 => [quads, 0, 0, 0, 0],
+                3 => [
+                    pairs == 1 ? 1 : 0,     //fifths
+                    pairs == 0 ? 1 : 0,     //quads                         
+                    0,                      //full house
+                    0,                      //triples
+                    0,                      //pairs
+                ],
+                2 => [
+                    triples == 1 ? 1 : 0,   //fifths
+                    pairs == 2 ? 1 : 0,     //quads
+                    0,                      //full house
+                    pairs == 1 ? 1 : 0,     //triples
+                    0,                      //pairs
+                ],
+                1 => [
+                    quads == 1 ? 1 : 0,     //fifths
+                    triples == 1 ? 1 : 0,   //quads
+                    pairs == 2 ? 1 : 0,     //full house
+                    pairs == 1 ? 1 : 0,     //triples
+                    pairs == 0 ? 1 : 0,     //pairs
+                ],
+                0 => [fifths, quads, fullHouse, triples, pairs],
+                _ => throw new Exception("Invalid number of jokers")
+            };
+        }).OrderDescending().ToArray();
+
+        for (int i = 0; i < games.Length; i++)
+        {
+            winnings += games[i].BidAmount * (i +1);
+        }
+
+        return winnings.ToString();
+    }
+
+    private IEnumerable<Game> ParseInput(string cardSet, Func<int[], int[]> createScoreBoard)
+    {
+        ResetReader();
+
+        string? line;
+        while ((line = _reader.ReadLine()) != null)
+        {
+            var parts = line.Split(' ');
+            var hand = parts[0].Select(c => cardSet.IndexOf(c)).ToArray();
+            var bid = int.Parse(parts[1]);
+            yield return new Game(hand, bid, createScoreBoard);
+        }
+    }
+
+    private class Game(int[] hand, int bidAmount, Func<int[], int[]> createScoreBoard) : IComparable<Game>
+    {
+        public int[] Hand { get; } = hand;
+        public int BidAmount { get; } = bidAmount;
+        public int CompareTo(Game? other)
+        {
+            if(other == null)
+            {
+                return -1;
+            }
+
+            var score = createScoreBoard(Hand);
+            var otherScore = createScoreBoard(other.Hand);
+
+            for (var i = 0; i < score.Length; i++)
+            {
+                var difference = otherScore[i] - score[i];
+                if(difference != 0)
+                {
+                    return difference;
+                }
+
+                if(score[i] > 0)
+                {
+                    break;
+                }
+            }
+
+            for (var i = 0; i < Hand.Length; i++)
+            {
+                var cardDiff = other.Hand[i] - Hand[i];
+                if (cardDiff != 0)
+                {
+                    return cardDiff;
+                }
+            }
+            
+            return 0;
+        }
+    }
+}
